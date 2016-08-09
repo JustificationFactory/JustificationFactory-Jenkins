8821e50acbe243ab1ce0fe1f2931951792466d7e
Clement <clement0210@gmail.com>
2016-08-09 10:20:36 +0200

    DEV refs #39 : add states

--- avek-engine/src/main/java/fr/axonic/avek/engine/instance/conclusion/EffectEnum.java
+++ avek-engine/src/main/java/fr/axonic/avek/engine/instance/conclusion/EffectEnum.java
@@ -1,8 +1,36 @@
 package fr.axonic.avek.engine.instance.conclusion;
 
+import fr.axonic.base.ARangedEnum;
+import fr.axonic.base.engine.AVarHelper;
+import fr.axonic.validation.exception.VerificationException;
+
+import java.util.Arrays;
+
 /**
  * Created by cduffau on 04/08/16.
  */
 public enum EffectEnum {
-    EFFICIENT, UNDESIRABLE, STRONGLY_UNDESIRABLE, UNKNOWN
+    EFFICIENT(EffectStateEnum.values()), UNDESIRABLE(EffectStateEnum.values()), STRONGLY_UNDESIRABLE(EffectStateEnum.values()), UNKNOWN(EffectStateEnum.UNKNOWN);
+
+
+    public ARangedEnum<EffectStateEnum> state;
+
+    EffectEnum(EffectStateEnum ... effectStateEnums ) throws VerificationException {
+        state=new ARangedEnum<>();
+        state.setLabel("Effect State");
+        state.setPath("fr.axonic.effectType");
+        state.setCode("state");
+        state.setRange(AVarHelper.transformToAVar(Arrays.asList(effectStateEnums)));
+    }
+
+    public ARangedEnum<EffectStateEnum> getState() {
+        return state;
+    }
+
+    private void setState(ARangedEnum<EffectStateEnum> state) {
+        this.state = state;
+    }
+    public void setStateValue(EffectStateEnum state) throws VerificationException {
+        this.state.setValue(state);
+    }
 }
