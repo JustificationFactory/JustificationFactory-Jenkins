de3fc9ee7e269d2354c72cdf48889408b3725964
Clement <clement0210@gmail.com>
2016-09-08 14:09:59 +0200

    DEV refs #50 : added equals methods on evidences

--- avek-engine/src/main/java/fr/axonic/avek/engine/conclusion/Conclusion.java
+++ avek-engine/src/main/java/fr/axonic/avek/engine/conclusion/Conclusion.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public class Conclusion<T extends Element> extends Evidence implements Cloneable {
 
+	protected List<Limit> limits;
+
 	public Conclusion(String name, T element) {
 		super(name, element);
 	}
@@ -20,7 +22,24 @@ public class Conclusion<T extends Element> extends Evidence implements Cloneable
 		// TODO Auto-generated constructor stub
 	}
 
-	protected List<Limit> limits;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Conclusion)) return false;
+		if (!super.equals(o)) return false;
+
+		Conclusion<?> that = (Conclusion<?>) o;
+
+		return limits != null ? limits.equals(that.limits) : that.limits == null;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + (limits != null ? limits.hashCode() : 0);
+		return result;
+	}
 
 	@Override
 	public Conclusion<T> clone() {
--- avek-engine/src/main/java/fr/axonic/avek/engine/evidence/Evidence.java
+++ avek-engine/src/main/java/fr/axonic/avek/engine/evidence/Evidence.java
@@ -12,6 +12,25 @@ public class Evidence<T extends Element> implements Cloneable{
 
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Evidence)) return false;
+
+        Evidence<?> evidence = (Evidence<?>) o;
+
+        if (name != null ? !name.equals(evidence.name) : evidence.name != null) return false;
+        return element != null ? element.equals(evidence.element) : evidence.element == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (element != null ? element.hashCode() : 0);
+        return result;
+    }
+
     public T getElement(){
         return element;
     }
